616a5e629ce05c7b6c2865f46427117890a46673
andre-bandeli <andre.lbandeli@gmail.com>
2022-03-27 15:26:16 -0300

    feat: implementando Dataloader - add user

---
+++ demo/src/main/java/com/br/Autentication/demo/configuration/Dataloader.java
@@ -0,0 +1,46 @@
+package com.br.Autentication.demo.configuration;
+
+
+import com.br.Autentication.demo.model.Roles;
+import com.br.Autentication.demo.model.User;
+import com.br.Autentication.demo.repository.RoleRepository;
+import com.br.Autentication.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+public class Dataloader implements CommandLineRunner {
+
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+        roleRepository.save(new Roles());
+        roleRepository.save(new Roles());
+
+        Roles adminRole = roleRepository.findByRole("ADMIN");
+        Roles userRole = roleRepository.findByRole("USER");
+
+        User adminUser = new User("admin@code.com", passwordEncoder.encode("password"), "Admin", "Super", "admin", true);
+        adminUser.setRoles(Arrays.asList(adminRole));
+        userRepository.save(adminUser);
+
+        User user = new User("user@code.com", passwordEncoder.encode("password"), "User", "Super", "user", true);
+        user.setRoles(Arrays.asList(userRole));
+        userRepository.save(user);
+
+
+    }
+}
--- demo/src/main/java/com/br/Autentication/demo/model/Roles.java
+++ demo/src/main/java/com/br/Autentication/demo/model/Roles.java
@@ -27,7 +27,6 @@ public class Roles {
         this.users = users;
     }
 
-
     public long getId() {
         return id;
     }
