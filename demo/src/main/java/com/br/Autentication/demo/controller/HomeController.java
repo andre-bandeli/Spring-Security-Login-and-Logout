b8c29768bd8946a355e94e88707dbcff87fcf12a
andre-bandeli <andre.lbandeli@gmail.com>
2022-03-27 12:41:03 -0300

    feat: implementando controller login

---
+++ demo/src/main/java/com/br/Autentication/demo/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.br.Autentication.demo.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+
+    @RequestMapping("/")
+    public String login() {
+        return "login";
+    }
+
+}
