62f8ea75a1c5b00922d0317f4daab8a2cedcf45a
andre-bandeli <andre.lbandeli@gmail.com>
2022-03-27 14:23:12 -0300

    feat: autenticacao login + logout

--- demo/src/main/java/com/br/Autentication/demo/SpringConfiguration.java
+++ demo/src/main/java/com/br/Autentication/demo/SpringConfiguration.java
@@ -6,26 +6,34 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 
 @Configuration
 @EnableWebSecurity
-public class SpringConfiguration extends WebSecurityConfiguration {
+public class SpringConfiguration extends WebSecurityConfigurerAdapter {
 
     @Bean
     public static BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated().and().formLogin();
+        http.authorizeRequests()
+                .antMatchers("/").access("hasAnyAuthority('USERS', 'ADMIN')")
+                .antMatchers("/admin").access("hasAuthority('ADMIN')")
+                .anyRequest().authenticated().and().formLogin().loginPage("/entrar").permitAll()
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login").permitAll();
     }
 
-
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("user").password(passwordEncoder().encode("password")).authorities("USER");
+        auth.inMemoryAuthentication().withUser("user").password(passwordEncoder().encode("password")).authorities("ADMIN");
     }
 
 
--- demo/src/main/java/com/br/Autentication/demo/controller/HomeController.java
+++ demo/src/main/java/com/br/Autentication/demo/controller/HomeController.java
@@ -2,15 +2,31 @@ package com.br.Autentication.demo.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
 
 @Controller
+@RequestMapping
 public class HomeController {
 
 
-    @RequestMapping("/")
-    public String login() {
-        return "login";
+    @RequestMapping("/entrar")
+    public ModelAndView login() {
+        ModelAndView mv = new ModelAndView("login");
+        return mv;
     }
 
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
 }
